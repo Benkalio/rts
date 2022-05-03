e30703251fb1bc9020e815ec1f4220e2d1b3c3d5
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-03 16:55:21 +0100

    trying out styles for the careers page

--- src/careers/CareerPage.tsx
+++ src/careers/CareerPage.tsx
@@ -1,25 +1,59 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import styled from 'styled-components';
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Header = styled.h1`
+  font-weight: strong;
+  font-size: 60px;
+`
+const Body = styled.div`
+  width: 500px;
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+` 
+const Joinus = styled.div`
+  display: grid;
+`
+const CareerBody = styled.a`
+  width: 100%;
+  background-color: lightgrey;
+  margin: 10px auto;
+  border: 0.1px solid lightgrey;
+  border-radius: 20px;
+  padding: 5px;
+  text-decoration: none;
+
+  &:hover {
+    background-color: darkgrey;
+  }
+`
+
+const Career = styled.div`
+  padding-left: 20px;
+  color: black;
+  outline: none;
+`
 
 const CareerPage = () => {
-  library.add(fas);
+  // library.add(fas);
 
   return (
-    <div>
+    <Body>
       <div>
-        <h1>
+        <Header>
           We believe
           that reading
           is Magic
-        </h1>
+        </Header>
         <p>
         And that we need new tools to keep that magic alive.
         Think about a moment when a piece of writing opened
         you up to a totally new understanding of your world.
         Or when a friend had you read something that ignited a conversation
         and forced you to articulate a belief you didn’t quite have the words for. 
-        <br />
-
         We’re building a tool to give you more of those moments.
         A tool that chooses agency over algorithm.
         Debate over noise. Quality over recency.
@@ -29,9 +63,9 @@ const CareerPage = () => {
         and many stellar angels, we set out to build something better.
         </p>
         <h2>
-        <span>We’re looking for creative,</span>
-        <span>ambitious people to help us</span>
-        <span>craft the future of reading.</span>
+          We’re looking for creative,
+          ambitious people to help us
+          craft the future of reading
         </h2>
         <p>
           You’ll fit right in if you want to take ownership of a product,
@@ -43,37 +77,29 @@ const CareerPage = () => {
           If that excites you, we should talk.
         </p>
       </div>
-      <div>
-        <ul>
-          <li>
-            <a href="/">
-              <div>
-                <h4>Design Engineer</h4>
-                <p>Fully Remote</p>
-              </div>
-              <div>
-                {/* <FontAwesomeIcon icon="square-chevron-right" /> */}
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <div>
-                <h4>Software Engineer, Backend</h4>
-                <p>Fully Remote</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <div>
-                <h4>Open Role</h4>
-                <p>Fully Remote</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Joinus>
+        <CareerBody href="#">
+          <Career>
+            <h4>Design Engineer</h4>
+            <p>Fully Remote</p>
+          </Career>
+          <Career>
+            <FontAwesomeIcon icon="square-chevron-right" />
+          </Career>
+        </CareerBody>
+        <CareerBody href="#">
+          <Career>
+            <h4>Software Engineer, Backend</h4>
+            <p>Fully Remote</p>
+          </Career>
+        </CareerBody>
+        <CareerBody href="#">
+          <Career>
+            <h4>Open Role</h4>
+            <p>Fully Remote</p>
+          </Career>
+        </CareerBody>
+      </Joinus>
       <div id="benefits">
         <h3>Benefits</h3>
         <ul>
@@ -88,7 +114,7 @@ const CareerPage = () => {
         <h3>We always love to chat.</h3>
         <p>For features requests, reach out at help@stoicbible.com</p>
       </div>
-    </div>
+    </Body>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom";
-// import CareerPage from "./careers/CareerPage";
-import EventComponent from "./events/EventComponent";
-import GuestList from "./state/GuestList";
-import UserSearch from "./refs/UserSearch";
+import CareerPage from "./careers/CareerPage";
+// import EventComponent from "./events/EventComponent";
+// import GuestList from "./state/GuestList";
+// import UserSearch from "./refs/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <UserSearch />
+      {/* <UserSearch />
       <GuestList />
-      <EventComponent />
-      {/* <CareerPage /> */}
+      <EventComponent /> */}
+      <CareerPage />
     </div>
   );
 };
